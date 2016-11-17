2cfa2ed855ff6235ef35fd1fee017a4e4cb25e04
Valery Tolstov <testautomail@mail.ru>
2016-11-18 01:50:19 +0300

    Pull data with archive

--- pull-startup.sh
+++ pull-startup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 NAME=${1:-startup}
+DIR=/opt/usr/home/owner/startup
+FILE=${DIR}/startup.tar.xz
 
 mkdir -vp $NAME
-sdb pull /opt/usr/home/owner/startup $NAME
+
+sdb pull $FILE $NAME
+pushd $NAME
+tar xvf startup.tar.xz
+rm startup.tar.xz
+popd
