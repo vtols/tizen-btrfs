17310d14e2ff09c0cda1d48d3001d23e5b1b3fb8
Valery Tolstov <testautomail@mail.ru>
2016-11-17 03:00:07 +0300

    Fix process id retrieval

--- startup.sh
+++ startup.sh
@@ -2,11 +2,8 @@
 
 WD=/opt/usr/home/owner
 DELAY=20
-WAIT=50
-COUNT=4
-if [ $1 ]; then
-    COUNT=$1
-fi
+WAIT=30
+COUNT=$1
 
 unit=startup.service
 unit_prefix=/etc/systemd/system
@@ -25,14 +22,17 @@ function check_counter() {
     if [ $NEW_COUNTER -eq 0 ]; then
         systemctl disable $unit
         rm counter.txt $unit_path startup.sh
-        chown -R owner:owner startup
+        chown -R owner:users startup
         exit
     fi
 }
 
 function startup_stat() {
     sleep $DELAY
-    systemd-analyze > $st_dir/startup.txt
+    systemd-analyze | awk '{print $4,$7,$10}' | \
+        sed -e 's/s//g' > $st_dir/startup.txt
+
+    dmesg > $st_dir/dmesg.txt
 }
 
 function startup_init() {
@@ -63,20 +63,34 @@ LAUNCH_SET=(
     org.tizen.chromium-efl.mini_browser
 )
 
+BIN_NAMES=(
+    calculator
+    ddktest
+    glrenderer
+    player
+    org.tizen.dpm-toolkit
+    mini_browser
+)
+
 function launch_stat() {
-    #clear_cache
     num_apps=${#LAUNCH_SET[@]}
     indices=$(seq 0 $(($num_apps-1)))
     for i in $indices;
     do
+        clear_cache
         app_name=${LAUNCH_SET[$i]}
         su --command="launch_app ${app_name}" owner
-        app_pid=$(ps aux | grep ${app_name} | head -n 1 | awk '{print $2}')
-        #app_pid=$(ps -C ${app} -o pid=)
+        app_pid=$(ps -C ${BIN_NAMES[$i]} -o pid=)
         echo "${app_name} ${app_pid}" >> $st_dir/pids.txt
     done
     sleep $WAIT
-    dlogutil -d *:D | grep prt_ltime > $st_dir/dlog.txt
+    ps aux > $st_dir/ps.txt
+    dlogutil -d *:D | grep prt_ltime | tee $st_dir/dlog_raw.txt | \
+        awk '{print $7, $12}' | sed -e 's/]//' > $st_dir/dlog.txt
+}
+
+function clear_cache() {
+    sync; echo 3 > /proc/sys/vm/drop_caches
 }
 
 cd $WD
