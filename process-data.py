59f7e5eae6404c36d22ead603768e3f28acbd21a
Valery Tolstov <testautomail@mail.ru>
2016-11-20 20:50:48 +0300

    Refactor data processing script

--- process-data.py
+++ process-data.py
@@ -8,18 +8,38 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 from pathlib import Path
 
+def plot_medians(name, y):
+    median = np.median(y)
+    x = list(range(len(y)))
+    y_median = [median for i in x]
+    fig, ax = plt.subplots()
+    data_line = ax.plot(x, y, 'o', label='Time')
+    median_line = ax.plot(x, y_median, label='Median time', linestyle='-')
+    plt.title(name)
+    plt.savefig(str(data_dir / (name + '.png')))
+    #plt.show()
+
+def plot_density(name, y):
+    plt.figure()
+    plt.hist(y, bins='auto')
+    plt.title(name + " (histogram)")
+    plt.savefig(str(data_dir / (name + '.hist.png')))
+
+def entries(name):
+    lines = (run_dir / name).open().readlines()
+    return list(map(str.split, lines))
+
+def entry_map(name):
+    return { k: v for [k, v] in entries(name) }
+
 data_dir = Path.cwd() / sys.argv[1]
 
 system_start_time = []
 apps_start_time = defaultdict(lambda: [])
 
-for run_dir in data_dir.iterdir():
-    def entries(name):
-        lines = (run_dir / name).open().readlines()
-        return list(map(str.split, lines))
+samples = 0
 
-    def entry_map(name):
-        return { k: v for [k, v] in entries(name) }
+for run_dir in data_dir.iterdir():
 
     if run_dir.is_dir():
         #print("Reading", run_dir)
@@ -32,25 +52,11 @@ for run_dir in data_dir.iterdir():
 
             system_time = float(entries('startup.txt')[0][2])
             system_start_time.append(system_time)
+            samples += 1
         except:
             print("Bad data in", run_dir)
 
-def plot_medians(name, y):
-    median = np.median(y)
-    x = list(range(len(y)))
-    y_median = [median for i in x]
-    fig, ax = plt.subplots()
-    data_line = ax.plot(x, y, label='Time', marker='o')
-    meidan_line = ax.plot(x, y_median, label='Median time', linestyle='--')
-    plt.title(name)
-    plt.savefig(str(data_dir / (name + '.png')))
-    #plt.show()
-
-def plot_density(name, y):
-    plt.figure()
-    plt.hist(y, bins='auto')
-    plt.title(name + " (histogram)")
-    plt.savefig(str(data_dir / (name + '.hist.png')))
+print("Total {} samples".format(samples))
 
 plot_medians('sys', system_start_time)
 for name, data in apps_start_time.items():
